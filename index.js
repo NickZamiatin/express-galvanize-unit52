d1530c4312abe2340747373bf6ffd3ec1e44a967
Nikita Zamiatin <4406015@gmail.com>
2018-05-16 11:11:07 -0400

    done with GET POST PUT DELETE

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const booksPath = path.join(__dirname, 'books.json')// to grab json folder we need use path and join and  _ _dirname
 const PORT = process.env.PORT || 3001;
 
 app.use(morgan('dev'));
