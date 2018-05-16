77b92648996f5181c596e86c4afc0c4e4d20f69d
Nikita Zamiatin <4406015@gmail.com>
2018-05-16 10:44:28 -0400

    Finish basic

---
+++ inde.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+app.get('/',(require, response)=>{
+  response.send('Welcome to the library')
+})
+
+app.listen(PORT, () => {
+	console.log(`library-express: Express application is listening on port ${PORT}...`);
+});
